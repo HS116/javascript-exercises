5336ebcdb3a180cfd33f33d5ac3588b28efcf5d1
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-04-24 22:02:52 +0200

    finished passing all tests lol

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return round1dp((temp - 32) * 5/9);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return round1dp(9/5 * temp + 32);
 };
+
+function round1dp(num){
+  return (Math.round(num * 10)) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
