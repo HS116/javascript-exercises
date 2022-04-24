d4b24263005b339166f9b1c7a89bef85312d67c4
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-04-24 21:34:49 +0200

    finished passing all test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    if (typeof startNum != "number" || typeof endNum != "number"){
+        return "ERROR";
+    }
+
+    if (startNum < 0 || endNum < 0){
+        return "ERROR";
+    }
+
+    let sum = 0;
+
+    if (startNum > endNum){
+        let temp = startNum;
+        startNum = endNum;
+        endNum = temp;
+    }
+
+    for (let i = startNum; i <= endNum; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
