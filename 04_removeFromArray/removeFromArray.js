83c02241a2959e15d597e4e0af541886e9433292
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-04-24 14:36:06 +0200

    passed first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, element) {
+    index = array.indexOf(element);
+    array.splice(index, 1)
+    return array;
 };
 
 // Do not edit below this line
