d38fa625f5ad7578803939bfef9d7f2e3a39d46d
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-04-24 14:26:31 +0200

    finished exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    chars = string.split('');
+    let reversedChars = [];
+    for (let i = chars.length - 1; i >= 0; i--){
+        reversedChars.push(chars[i]);
+    }
 
+    return reversedChars.join('');
 };
 
 // Do not edit below this line
