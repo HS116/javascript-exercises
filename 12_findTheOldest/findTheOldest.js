7cc515e9ef88fcc5c6a10c10140980642801912e
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-05-01 21:55:22 +0200

    finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function calcAge(person){
+    if (person.yearOfDeath){
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return new Date().getFullYear() - person.yearOfBirth;
+    }
+}
 
+
+const findTheOldest = function(people) {
+    return people.sort((firstPerson, secondPerson) => calcAge(firstPerson) < calcAge(secondPerson) ? 1 : -1)[0];
 };
 
 // Do not edit below this line
