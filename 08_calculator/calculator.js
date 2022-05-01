ef7998f08b60a6e21f43625472e247a10c1e88ce
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-05-01 20:59:46 +0200

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce((total, currentNum) => total + currentNum, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const product = array.reduce((total, currentNum) => total * currentNum, 1);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let total = 1;
+	for (let i = 1; i <= a; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
