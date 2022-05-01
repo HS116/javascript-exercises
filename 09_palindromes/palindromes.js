271266bb59b0e7fa269567431572b7a78dd0606d
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-05-01 21:32:34 +0200

    finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,32 @@
-const palindromes = function () {
+function isLowerAlpha(char){
+    return char >= "a" && char <= "z";
+}
+
+
+const palindromes = function (word) {
+    word = word.toLowerCase();
+    let start = 0;
+    let end = word.length - 1;
+
+    while (start <= end){
+
+        while (!isLowerAlpha(word[start])){
+            start++;
+        }
+
+        while (!isLowerAlpha(word[end])){
+            end--;
+        }
+
+        if (word[start] != word[end]){
+            return false;
+        }
+
+        start++;
+        end--;
+    }
+
+    return true;
 
 };
 
