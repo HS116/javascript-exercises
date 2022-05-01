cf3a31bc655ff074b0744a3223a0e94833718d40
Neil Albuquerque <neilalbuquerque2002@gmail.com>
2022-05-01 21:44:48 +0200

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+    n = parseInt(n);
+
+    if (n <= 0){
+        return "OOPS";
+    } else if (n == 1){
+        return 1;
+    } else if (n == 2){
+        return 1;
+    } else {
+        let a = 1;
+        let b = 1;
+        for (let i = 3; i <= n; i++){
+            let temp = a;
+            a = b;
+            b = b + temp;
+        }
+
+        return b;
+    }
 
 };
 
